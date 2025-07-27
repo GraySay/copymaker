be17575c57ac3a0cfd788d1c866b1c1d8634742e
hex808080 <121229235+GraySay@users.noreply.github.com>
2025-07-28 02:49:12 +0300

    Update and rename config.example.js to config.js

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 // Пример конфигурации - скопируйте этот файл в config.js и заполните своими данными
 window.CONFIG = {
   GOOGLE: {
-    CLIENT_ID: "your-client-id-here.apps.googleusercontent.com",
-    API_KEY: "your-api-key-here", 
+    CLIENT_ID: "__CLIENT_ID__",
+    API_KEY: "__API_KEY__", 
     DISCOVERY_DOC: "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
     SCOPES: "https://www.googleapis.com/auth/drive.readonly"
   }
